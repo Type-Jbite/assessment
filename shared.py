1d272405f49197b1477f8de2a9ad380152340912
Type-Jbite <115145227+Type-Jbite@users.noreply.github.com>
2025-10-08 14:11:37 +0300

    Add files via upload

---
+++ shared.py
@@ -0,0 +1,22 @@
+# shared.py
+
+import math
+
+# === КОНФИГУРАЦИЯ ===
+
+CLASSROOM_LAT = 55.728900
+CLASSROOM_LON = 37.562056
+ALLOWED_RADIUS = 0.1
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    R = 6371
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+    a = math.sin(dlat/2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = R * c
+    return distance
